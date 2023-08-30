bbca1b01c16df8b7b1ffd1ebd6af6afe52e217e9
Varun <sharmavarun.1912@gmail.com>
2023-08-30 16:09:57 +0530

    pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Portfolio_3D",
   plugins: [react()],
-})
+});
