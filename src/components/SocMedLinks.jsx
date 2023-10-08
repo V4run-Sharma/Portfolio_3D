f31da429474783698b021b694b4d80a94ad3460c
Varun <sharmavarun.1912@gmail.com>
2023-10-08 10:11:20 +0530

    fixes

--- src/components/SocMedLinks.jsx
+++ src/components/SocMedLinks.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import { linkedin, github } from "../assets";
 
 const SocMedLinks = () => {
   return (
-    <div className="fixed flex items-center rounded-[7px] p-[2px] green-pink-gradient right-[-3.30rem] hover:right-0 sm:active:right-0 transition-all ease-in-out top-20 z-20">
+    <div className="fixed flex items-center rounded-[7px] p-[2px] green-pink-gradient right-[-2.7rem] hover:right-[0.04rem] sm:active:right-0 transition-all ease-in-out top-20 z-20">
       <div className="w-2 ml-[-0.1rem] text-2xl">|</div>
-      <div className="rounded-md w-fit m-[-0.5px] h-fit flex flex-col space-y-2 p-2 bg-tertiary ">
+      <div className="rounded-md w-fit m-[-0.5px] h-fit flex flex-col items-center space-y-[0.2rem] p-1 bg-tertiary ">
         <a
           href="https://linkedin.com/in/varun-sharma-4261bb1a0"
           target="_blank"
-          className="w-8 h-8 bg-[url('./linkedin.png')] bg-cover"
-        ></a>
-        <a
-          href="https://github.com/V4run-Sharma/"
-          target="_blank"
-          className="w-9 h-9 bg-[url('./github.png')] bg-cover"
-        ></a>
+        >
+          <div
+            className="w-8 h-8 bg-cover"
+            style={{ backgroundImage: `url(${linkedin})` }}
+          ></div>
+        </a>
+        <a href="https://github.com/V4run-Sharma/" target="_blank">
+          <div
+            className="w-9 h-9 bg-cover"
+            style={{ backgroundImage: `url(${github})` }}
+          ></div>
+        </a>
       </div>
     </div>
   );
